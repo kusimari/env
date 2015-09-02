75c6fb37343f51b3b2aa82e6c86e254a2dd33857
Santhosh G Ramakrishna <kusimari.share@gmail.com>
2015-09-02 14:06:57 +0530

    Update kusimari-emacs.el

--- kusimari-emacs.el
+++ kusimari-emacs.el
@@ -22,9 +22,9 @@
 ;; COLORS and VISUALS
 (setq inhibit-startup-screen t) ;; no startup or splash
 
+(global-hl-line-mode +1)
 (install-require 'zenburn-theme)
 (load-theme 'zenburn t)
-(global-hl-line-mode +1)
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
