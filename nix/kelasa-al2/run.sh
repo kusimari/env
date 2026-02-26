65ead2d356829d3f69fb5286e83c4a49da3030c0
Santhosh Ramakrishna <gorantls@amazon.com>
2026-02-26 10:12:06 -0800

    Add backup flag to home-manager init to handle existing files

--- nix/kelasa-al2/run.sh
+++ nix/kelasa-al2/run.sh
@@ -10,9 +10,9 @@
 # Platform-specific configuration for common-run.sh
 # Linux sed uses -i without additional flag (don't export SED_INPLACE_FLAG)
 
-# For AL2: Use nix run command for initial setup (after fixing nixbld group)
-export NIX_COMMAND='nix --extra-experimental-features "nix-command flakes" run home-manager/master -- init --switch "$FLAKE_DIR#kelasa-al2"'
-export NIX_ECHO_MESSAGE="Running home-manager init with nix run..."
+# For AL2: Use nix run command for initial setup with backup
+export NIX_COMMAND='nix --extra-experimental-features "nix-command flakes" run home-manager/master -- init --switch -b backup "$FLAKE_DIR#kelasa-al2"'
+export NIX_ECHO_MESSAGE="Running home-manager init with backup..."
 
 # Standard command (use after initial setup)
 # export NIX_COMMAND='home-manager switch --flake "$FLAKE_DIR#kelasa-al2"'
