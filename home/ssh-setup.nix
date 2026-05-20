c05e7a841dc66321c9282ba43e4c8573e7876cc6
kusimari <kusimari@gmail.com>
2026-05-20 16:21:33 -0700

    feat: add mosh for persistent remote connections
    Installs mosh (client + server) as Tier 1 across all envKinds and
    adds `mw <host> <session>` shell function for quick tmux attach over
    mosh.

--- home/ssh-setup.nix
+++ home/ssh-setup.nix
@@ -1,6 +1,8 @@
-# SSH Setup - GitHub config and activation script
+# SSH Setup - GitHub config, activation script, and mosh (persistent connections)
 { config, lib, pkgs, ... }:
 {
+  home.packages = [ pkgs.mosh ];
+
   # GitHub SSH config included by system ~/.ssh/config
   home.file.".ssh/config_nix".text = ''
     Host github.com
@@ -11,5 +13,16 @@
   # Runs during home-manager switch to setup SSH (adds Include directive, fetches keys)
   home.activation.setupSSHConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
     bash ${./ssh-setup.sh}
+  '';
+
+  # mw: mosh to a remote host and attach (or create) a tmux session
+  programs.zsh.initContent = lib.mkOrder 600 ''
+    mw() {
+      if [[ $# -ne 2 ]]; then
+        echo "Usage: mw <host> <session>" >&2
+        return 1
+      fi
+      mosh "$1" -- tmux new-session -As "$2"
+    }
   '';
 }
