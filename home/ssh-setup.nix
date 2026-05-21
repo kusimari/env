969d5d6d5ddf952365cd034a604dc5e9206de87b
Santhosh G Ramakrishna <kusimari@gmail.com>
2026-05-20 22:07:45 -0700

    feat: autossh + tmux tab titles for persistent remote connections
    autossh monitors SSH and auto-reconnects through the proxy; tmux
    keeps sessions alive server-side during the gap.
    
    Also adds ServerAliveInterval/CountMax to config_nix so dead
    connections are detected within ~90s.

--- home/ssh-setup.nix
+++ home/ssh-setup.nix
@@ -1,11 +1,19 @@
-# SSH Setup - GitHub config and activation script
+# SSH Setup - GitHub config, activation script, and autossh (persistent connections)
 { config, lib, pkgs, ... }:
 {
-  # GitHub SSH config included by system ~/.ssh/config
+  # autossh -M 0 -t <remote> -- tmux new-session -As <session-name>
+  home.packages = [ pkgs.autossh ];
+
+  # Included by system ~/.ssh/config via "Include ~/.ssh/config_nix"
   home.file.".ssh/config_nix".text = ''
     Host github.com
       IdentityFile ~/.ssh/github_id
       UserKnownHostsFile ~/.ssh/known_hosts ~/.ssh/known_hosts_nix
+
+    # autossh -M 0 relies on these to detect dead connections (~90s)
+    Host *
+      ServerAliveInterval 30
+      ServerAliveCountMax 3
   '';
 
   # Runs during home-manager switch to setup SSH (adds Include directive, fetches keys)
